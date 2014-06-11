be66f0ed0f847c669d59de5982a4b4f1cb3bcba5
David Kyle <David.Kyle@prelert.com>
2014-06-11 15:58:05 +0100

    Add setup.py for distributing core packages

---
+++ setup.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+############################################################################
+#                                                                          #
+# Copyright 2014 Prelert Ltd                                               #
+#                                                                          #
+# Licensed under the Apache License, Version 2.0 (the "License");          #
+# you may not use this file except in compliance with the License.         #
+# You may obtain a copy of the License at                                  #
+#                                                                          #
+#    http://www.apache.org/licenses/LICENSE-2.0                            #
+#                                                                          #
+# Unless required by applicable law or agreed to in writing, software      #
+# distributed under the License is distributed on an "AS IS" BASIS,        #
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. #
+# See the License for the specific language governing permissions and      #
+# limitations under the License.                                           #
+#                                                                          #
+############################################################################
+"""
+Prelert Python packages
+"""
+
+from distutils.core import setup
+setup(name='Prelert',
+      description='Python packages for Prelert',
+      version='0.3',
+      license='Apache License, Version 2.0',
+      url='https://github.com/prelert/engine-python',
+      packages=['prelert', 'prelert.engineApiClient'],
+      )
