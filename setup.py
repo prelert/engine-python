6ad9cd76ea9f36539987317accb3c8b7a2b3fd9d
David Kyle <david.kyle@prelert.com>
2015-07-08 15:30:42 +0100

    Set version number to 1.5.0

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ Prelert Python packages
 from distutils.core import setup
 setup(name='Prelert',
       description='Python packages for Prelert',
-      version='1.4',
+      version='1.5.0',
       license='Apache License, Version 2.0',
       url='https://github.com/prelert/engine-python',
       packages=['prelert', 'prelert.engineApiClient'],
