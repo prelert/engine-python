43e634620f2f7844285c0ae6ac2624ae71bd5a0a
David Roberts <dave@prelert.com>
2016-03-23 10:54:26 +0000

    Update version number

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 ############################################################################
 #                                                                          #
-# Copyright 2014 Prelert Ltd                                               #
+# Copyright 2014-2016 Prelert Ltd                                          #
 #                                                                          #
 # Licensed under the Apache License, Version 2.0 (the "License");          #
 # you may not use this file except in compliance with the License.         #
@@ -23,7 +23,7 @@ Prelert Python packages
 from distutils.core import setup
 setup(name='Prelert',
       description='Python packages for Prelert',
-      version='1.5.0',
+      version='2.0.0',
       license='Apache License, Version 2.0',
       url='https://github.com/prelert/engine-python',
       packages=['prelert', 'prelert.engineApiClient'],
